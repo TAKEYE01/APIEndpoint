c1a5773576515c6c7f393dbca5635a0fbc734a7e
Tanner <40257656+TAKEYE01@users.noreply.github.com>
2022-05-13 22:14:48 -0400

    Added get Hello World and get Hello my name is /name/:name

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.route('/').get((req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/name/:name', (req, res) => {
+    res.send('Hello my name is ' + req.params.name)
+})
+
+app.post('/post', (req, res) => {
+    res.send('Posted')
 })
 
 app.listen(port, () => {
